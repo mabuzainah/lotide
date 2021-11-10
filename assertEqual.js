82acac1c2d3db6dd135c4d6b8f18ac43da62cca5
Mohammed Abu-Zeinah <mabuzainah@gmail.com>
2021-11-09 19:54:44 -0500

    Create assertEqual.js
    Created assertEqual function for Lotide app

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+    if (actual===expected){
+        return console.log("Assertion Passed: " + actual + " === " + expected);
+    } else return console.log("Assertion Failed: " + actual + " !=== " + expected);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1,3);
+assertEqual("Bootcamp","Bootcamp");
