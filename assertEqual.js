d7c63c4d301350cd098ab59f82f865f99a435b76
Mohammed Abu-Zeinah <mabuzainah@gmail.com>
2021-11-09 20:12:40 -0500

    Add Emoji
    Added Emojis to Console Log output

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,12 @@
+//Emoji code
+const mad = String.fromCodePoint(0x1F621);
+const wave = String. fromCodePoint(0x1F44B)
+
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
     if (actual===expected){
-        return console.log("Assertion Passed: " + actual + " === " + expected);
-    } else return console.log("Assertion Failed: " + actual + " !=== " + expected);
+        return console.log(wave,wave,wave,"Assertion Passed: " + actual + " === " + expected);
+    } else return console.log(mad,mad,mad,"Assertion Failed: " + actual + " !=== " + expected);
 };
 
 // TEST CODE
