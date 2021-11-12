691503ff4e9de0c3322be188feeaf2fd97f1ddd7
Mohammed Abu-Zeinah <mabuzainah@gmail.com>
2021-11-11 20:31:17 -0500

    implmented middle.js
    Function that returns the middle of an array, for both even and odd. If array length < 3 returns empty array.

--- middle.js
+++ middle.js
@@ -1,3 +1,16 @@
+const eqArrays = function (array1, array2){
+    if (array1.length !== array2.length){
+        return false;
+    } else {
+        for (var i=0; i<array1.length; i++){
+            if (array1[i]===array2[i]){
+                return true;
+            } return false;
+        }
+    }
+};
+
+
 const assertArraysEquals = function (arr1, arr2){
     if ((arr1 !== undefined) && (arr2 !== undefined)){
         eqArrays (arr1,arr2);
@@ -6,11 +19,33 @@ const assertArraysEquals = function (arr1, arr2){
     }
 }
 
-const eqArrays = function (array1, array2){
-    if (array1.length !== array2.length){
-        return false;
-    } else {
-        array1.every((v, i) => v === array2[i])
-    }
-};
+// ACTUAL FUNCTION
+const middle = function(array) {
+    /*For arrays with one or two elements, there is no middle. Return 
+    an empty array. For arrays with odd number of elements,
+    an array containing a single middle element should be returned.
+    For arrays with an even number of elements, an array 
+    containing the two elements in the middle should be returned.
+    */
+    
+  const blankArr = [];
+  if (array.length < 3){
+      return blankArr;
+  }  
+  if (array.length % 2 === 1){
+      return [array[Math.floor(array.length / 2)]];
+  } else {
+    return [array[(array.length / 2) - 1], array[array.length / 2]];
+  }
+  
+}
 
+
+/* TEST CODE
+console.log(middle([1]));
+console.log(middle([1,2]));
+console.log(middle([1,2,3]));
+console.log(middle([1,2,3,4]));
+console.log(middle([1,2,3,4,5,6,7,8,9]));
+console.log(middle([1,2,3,4,5,6,7,8]));
+*/
