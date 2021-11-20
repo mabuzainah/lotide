4579171939bf3c604161d8834267d33edc051af1
Mohammed Abu-Zeinah <mabuzainah@gmail.com>
2021-11-20 14:10:09 -0500

    Create assertArraysEqualsTest,js file
    File that contains the test code for the JS file (assertArraysEqual.js)

---
+++ test/assertArraysEqualsTest.js
@@ -0,0 +1,5 @@
+//importing the assertArraysEqual.js file from the directory
+const assertArraysEquals = require('../assertArraysEqual');
+
+console.log(assertArraysEquals("1,2,3","1,2,3"));
+console.log(assertArraysEquals("1,2,3,4","1,2,3"));
