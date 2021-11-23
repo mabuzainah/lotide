5c5b9d6b3b2498af8dd59fd99c32ff5c04d5645d
Mohammed Abu-Zeinah <mabuzainah@gmail.com>
2021-11-23 18:48:31 -0500

    Update middleTest.js
    Using Mocha Chai for testing instead of Console.Log outputting the results of the function calls

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,12 +1,27 @@
 //importing the middle.js and assertArraysEqual.js file from the directory. To allow usage of middle function and assertArraysEquals
 const middle = require('../middle');
-const assertArraysEquals = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-// TEST CODE
-//console.log(assertArraysEquals([1],middle([1])));
-console.log(middle([1]));
-console.log(middle([1,2]));
-console.log(middle([1,2,3]));
-console.log(middle([1,2,3,4]));
-console.log(middle([1,2,3,4,5,6,7,8,9]));
-console.log(middle([1,2,3,4,5,6,7,8]));
+
+// test code using Mocha-Chai
+
+describe("#middle", () => {
+    it("returns [] for [1]", () => {
+        assert.deepEqual(middle([1]), []);
+    });
+    it("returns 2 for [1,2,3]", () => {
+      assert.deepEqual(middle([1,2,3]), [ 2 ]);
+    });
+    it("returns [] for [1,2]", () => {
+        assert.deepEqual(middle([1,2]), []);
+    });
+    it("returns [ 2, 3] for [1,2,3,4]", () => {
+        assert.deepEqual(middle([1,2,3,4]), [ 2, 3 ]);
+    });
+    it("returns 5 for [1,2,3,4,5,6,7,8,9]", () => {
+        assert.deepEqual(middle([1,2,3,4,5,6,7,8,9]), [ 5 ]);
+    });
+    it("returns [ 4, 5 ] for [1,2,3,4,5,6,7,8]", () => {
+        assert.deepEqual(middle([1,2,3,4,5,6,7,8]), [ 4 , 5 ]);
+    });  
+  });
