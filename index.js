c38e832efa44bffec4be5b7eea434ef4d3647838
Mohammed Abu-Zeinah <mabuzainah@gmail.com>
2021-11-23 19:04:53 -0500

    Update index.js
    adjusted typo in line 6 and fixed incorrect declaration for assertArraysEquals

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 const head   = require('./head');
 const middle = require('./middle');
 const eqArrays = require('./eqArrays');
-const assertArraysEqual = require('./assertArraysEqual');
-const assertEqual = require('../assertEqual');
+const assertArraysEquals = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
 
 module.exports = {
   head:   head,
